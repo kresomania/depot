9341f412695ff605b764afe41b2da067e5378880
Kresib <kresimir.bagaric@gmail.com>
2011-03-19 22:37:43 +0100

    husi... kapitel 6

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+Product.delete_all
+
+Product.create(:title => 'Kebab',
+  :description =>
+    %{<p>
+      Frische Fleisch und viel Scharf. Gut und gesund fuer alles.
+      </p>},
+  :image_url => '/images/kebab.jpg',
+  :price => 49.90)
+  
+Product.create(:title => 'Pepito',
+  :description =>
+    %{<p>
+      Feine Pouletfiljet in schoene Brot. Gut mit oder ohne scharf und Tomaten immer gut.
+      </p>},
+  :image_url => '/images/pepito.jpg',
+  :price => 19.11)
+    
+Product.create(:title => 'Falafel',
+  :description =>
+    %{<p>
+      Fuer kleine vegetarische Tiere.
+      </p>},
+  :image_url => '/images/falafel.jpg',
+  :price => 12.90)
